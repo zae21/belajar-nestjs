e9f69530bfdd334503833dd4813197edc195299b
zae21 <prayoga96@gmail.com>
2022-08-09 21:53:57 +0700

    add bookmark code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
     AuthModule,
     UserModule,
-    BookmarkModule,
-    PrismaModule
+    PrismaModule,
+    BookmarkModule
   ],
 })
 export class AppModule {}
